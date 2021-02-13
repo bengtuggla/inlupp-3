ec4833c7237de96e2a8ca85ef9911082c943f74f
Bengt Uggla <bengt.uggla@gmail.com>
2021-02-13 13:00:36 +0100

    Success removing user from page AND array!

--- script.js
+++ script.js
@@ -22,30 +22,32 @@ const allInputs = document.querySelectorAll('input')
  }
 } */
 
+//VALIDATE TEXT INPUTS
 const validate = (id)=> {
  let input = document.querySelector(id);
  let error = document.querySelector(id + '-error');
 
  if(input.value.trim() === ''){
-  error.innerText = 'Måste fyllas i med text!'
-  input.classList.remove('is-valid');
-  input.classList.add('is-invalid')
-  input.focus();
+   error.innerText = 'Måste fyllas i med text!'
+   input.classList.remove('is-valid');
+   input.classList.add('is-invalid')
+   input.focus();
   return false;
  }else if(input.value.trim().length < 2){
-  error.innerText = 'Måste fyllas i med minst två tecken!'
-  input.classList.remove('is-valid');
-  input.classList.add('is-invalid')
-  input.focus();
+   error.innerText = 'Måste fyllas i med minst två tecken!'
+   input.classList.remove('is-valid');
+   input.classList.add('is-invalid')
+   input.focus();
   return false;
  }else{
-  error.innerText = '';
-  input.classList.remove('is-invalid');
-  input.classList.add('is-valid');
-  return true;
+   error.innerText = '';
+   input.classList.remove('is-invalid');
+   input.classList.add('is-valid');
+   return true;
  }
 }
 
+//VALIDATE EMAILS
 const validateEmail = (_email) => {
   let error = document.querySelector('#email-error');
 
@@ -64,6 +66,8 @@ const validateEmail = (_email) => {
     return false;
   }
 }
+
+//CHECK UNIQUE EMAILS
 const checkUniqueEmail = (__email) => {
   let error = document.querySelector('#email-error');
   for(i=0; i< user.length; i++){
@@ -76,11 +80,18 @@ const checkUniqueEmail = (__email) => {
  }
 }
 
+/* 
+EVENT ON FORM 
+ -> Create Users
+ -> Invoke checkUniqueEmails function 
+ -> PrintOut Users
+*/
+
 form.addEventListener('submit', (e)=>{
- e.preventDefault();
- validate('#firstName');
- validate('#lastName');
- validateEmail(email);
+  e.preventDefault();
+  validate('#firstName');
+  validate('#lastName');
+  validateEmail(email);
 
  if(validate('#firstName') && validate('#lastName') && validateEmail(email)){
   //Nu kan vi skicka in användarobjektet i en array + displaya på sidan
@@ -95,56 +106,81 @@ form.addEventListener('submit', (e)=>{
     lastName: lastName,
     email: email,
    }
-    // checkUniqueEmail(email)
-    if(!checkUniqueEmail(email)){
-     user.push(user2)
-    }else{
-     email.classList.add('is-invalid');
-    }
-   
+    // checkUniqueEmail(email) & create users
+       if(!checkUniqueEmail(email)){
+        user.push(user2)
+       }else{
+        email.classList.add('is-invalid');
+       }
+    // end of checkUniqueEmail(email)
+
+    // Invoke PrintOut Users
    printList();
- 
-  form.reset();
+   form.reset();
  }else {
   console.log('Funkade inte');
  }
- // validateFirstName(e.currentTarget)
-
- 
-
- /* const user2 = 
-  {
-   firstName: firstName,
-   lastName: lastName,
-   email: email,
-  }
- 
- let testArray = [];
- testArray += testArray.push(user2);
-
-
-console.log(testArray); */
-
 
 })
 
+
+
+//Print out users
 const printList = ()=>{
  userOutput.innerHTML = '';
 
- user.forEach(item =>{
-  userOutput.innerHTML += 
+  user.forEach(item =>{
+    //Testa att lägga till ett unikt id på <h6> för varje varv
+    // Id:t ska dean matchas mot den <h6> som har en removeknapp som syskon.
+    
+    console.log(item.id);
+    
+   userOutput.innerHTML += 
 
-    ` <div class="eachUser bg-white py-2 d-flex justify-content-between mb-3 rounded shadow">
-            <div class="userData px-1">
-              <h4>${item.firstName} ${item.lastName}</h4>
-              <h6>${item.email}</h6>
-            </div>
-            <div class="buttons d-flex align-items-center mx-3">
-             <button class="btn-danger"><i class="fas fa-user-slash"></i></button>
-             <button class="btn-info"><i class="fas fa-user-edit"></i></button>
-            </div>
-       </div>`
- })
-
+     ` <div class="eachUser bg-white py-2 d-flex justify-content-between mb-3 rounded shadow">
+             <div class="userData px-1">
+               <h4>${item.firstName} ${item.lastName}</h4>
+               <h6>${item.email}</h6>
+             </div>
+             <div class="buttons d-flex align-items-center mx-3">
+              <button class="btn-danger"><i class="fas fa-user-slash"></i></button>
+              <button class="btn-info"><i class="fas fa-user-edit"></i></button>
+             </div>
+        </div>`
+        
+  })
 }
-// printList();
+
+// Remove user from userOutput
+userOutput.addEventListener('click', (e)=> {
+  if(e.target && e.target.classList[1] == 'fa-user-slash'){
+     e.target.parentNode.parentNode.parentNode.remove();
+  }
+
+  if(e.target.classList[1] == 'fa-user-slash'){
+    let dynamicEmail = e.target.parentNode.parentNode.parentNode.children[0].lastElementChild.innerText;
+    user.forEach((item, index)=>{
+      let i = index;
+      console.log(i);
+      if(dynamicEmail === item.email){
+        console.log(`  ${item.email}  ${i} removed from database and userList`);
+        console.log(user);
+        user.splice(i, 1)
+        console.log(user);
+
+      }
+    })
+    
+    // console.log(dynamicEmail);
+  }
+
+
+
+ /*  if(e.target.parentNode.parentNode.parentNode.children[0].lastElementChild.innerText === user[1].email){
+    console.log('SAAAMMMA');
+  }
+ */
+   
+    
+    
+})
